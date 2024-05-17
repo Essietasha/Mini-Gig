69ee4f12e89e97aa2c79db9673fbe207020f8d60
Olukoya Esther <estherbose18@gmail.com>
2024-05-17 01:23:50 +0100

    Mini Gig

---
+++ script.js
@@ -0,0 +1,34 @@
+function home () {
+    window.location.href = "index.html";
+}
+function goToMagazine () {
+    window.location.href = "mag.html";
+}
+function goToPiano () {
+    window.location.href = "piano.html";
+}
+function goToCitySkyline () {
+    window.location.href = "cityskyline.html";
+}
+function goToPenguin () {
+    window.location.href = "penguin.html";
+}
+function goToBalanceSheet () {
+    window.location.href = "balancesheet.html";
+}
+function goToProductLPage () {
+    window.location.href = "productlandingpage.html";
+}
+function goToFerrisWheel () {
+    window.location.href = "ferriswheel.html";
+}
+function goToCatPainting () {
+    window.location.href = "catpainting.html";
+}
+function goToForm () {
+    window.location.href = "form.html";
+}
+function goToNutrition () {
+    window.location.href = "nutrition.html";
+}
+
